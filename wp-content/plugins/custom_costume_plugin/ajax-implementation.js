77e05935bf2ccfa3ed1e8f756d81b97b7006e7e2
adam <adam@teather>
2014-04-18 21:53:42 +0100

    Synced with main

---
+++ wp-content/plugins/custom_costume_plugin/ajax-implementation.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+jQuery(document).ready(function() {
+    jQuery('#createacostume').submit(function( event ) {
+        event.preventDefault();
+        
+        var aForm = jQuery(this);
+        var bForm = aForm.serializeArray();
+        bForm.push({name: 'action', value: 'myAjaxFunction'});
+        jQuery.ajax({
+            url: ajaxurl,
+            type: "POST",
+            data: bForm,
+//            dataType: "json",
+            
+            
+            success: function(resp) {
+//                console.log(resp);
+                alert("Thank you for your post. We will review it and approve it shortly" + resp);
+
+            },
+            error: function( req, status, err ) {
+            	alert( 'something went wrong, Status: ' + status + ' and error: ' + err );
+            }
+        })
+//                .done(function(secResp) {
+//            alert("second response: " + secResp);
+//        })
+                .fail(function() {
+            alert("error");
+        })
+        ;
+        return false;
+    });
+});
---
+++ wp-content/plugins/custom_costume_plugin/test.php
@@ -0,0 +1,39 @@
+<?php
+
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
+    echo('Starting script');
+    
+    //var_dump(gd_info());
+    phpinfo();
+    
+    echo getcwd() . "<br>";
+    
+    $im = imagecreatefromjpeg(getcwd() . '/images/background.jpg');
+    
+    
+    
+//    $ims = imagescale($im, 1500, 1190, IMG_BICUBIC_FIXED);
+    
+    
+    
+    
+//    $im2 = imagecreatefromjpeg(getcwd() . '/images/head.jpg');
+    
+//    $icm = imagecopymerge( $im , $im2 , 31 , 400 , 0 , 0 , imagesx($im2) , imagesy($im2) , 100 );
+//    
+//    echo('Image merged ');  
+//    
+//    $success = imagejpeg($icmm, getcwd() . '/images/test.jpeg');
+
+echo('Image saved <br>');    
+    
+    imagedestroy($im);
+    imagedestroy($ims);
+    imagedestroy($im2);
+    imagedestroy($icm);
+    
+    die(0);
+    ?>
