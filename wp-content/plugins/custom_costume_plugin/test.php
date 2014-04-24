5ccb32f9e89f1c4da882d9ef619a8e404a41ef68
adam <adam@teather>
2014-04-24 08:06:10 +0100

    Had merg working but now its not

--- wp-content/plugins/custom_costume_plugin/test.php
+++ wp-content/plugins/custom_costume_plugin/test.php
@@ -11,28 +11,40 @@ error_reporting(-1);
     
     echo getcwd() . "<br>";
     
-    $im = imagecreatefromjpeg(getcwd() . '/images/background.jpg');
+//    $im = imagecreatefromjpeg(getcwd() . '/images/background.jpg');
+    $insertfile_id = imageCreateFromJPEG(getcwd() . '/images/head.jpg');
+    $sourcefile_id = imageCreateFromJPEG(getcwd() . '/images/background.jpg');
     
+    $sourcefile_id2 = imagescale($sourcefile_id, 1500, 1190, IMG_BICUBIC_FIXED);
     
+//    $im2 = imagecreatefromjpeg(getcwd() . '/images/head.jpg');
+    $sourcefile_width=imageSX($sourcefile_id2);
+    $sourcefile_height=imageSY($sourcefile_id2);
+    $insertfile_width=imageSX($insertfile_id);
+    $insertfile_height=imageSY($insertfile_id); 
     
-    $ims = imagescale($im, 1500, 1190, IMG_BICUBIC_FIXED);
-    
-    
-    
-    
-    $im2 = imagecreatefromjpeg(getcwd() . '/images/head.jpg');
-    
-    imagecopymerge( $im , $im2 , 31 , 400 , 0 , 0 , imagesx($im2) , imagesy($im2) , 100 );
+//    imagecopymerge( $im , $im2 , 31 , 400 , 0 , 0 , imagesx($im2) , imagesy($im2) , 100 );
+    $dest_x = ( $sourcefile_width / 2 ) - ( $insertfile_width / 2 );
+    $dest_y = ( $sourcefile_height / 2 ) - ( $insertfile_height / 2 ); 
+    imageCopyMerge($sourcefile_id, $insertfile_id,$dest_x,$dest_y,0,0,$insertfile_width,$insertfile_height,100);
     
     echo('Image merged ');  
     
-    $success = imagejpeg($im, getcwd() . '/images/test.jpg', 75);
+    $success = imagejpeg($sourcefile_id, getcwd() . '/images/test.jpg', 75);
 
 echo('Image saved <br>');    
+print "original:<hr><img src=\"$sourcefile_id\" width=\"300\"><br><br><br>new:<hr><img src=\"getcwd() . '/images/test.jpg'\">
+<br>source width = $sourcefile_width
+<br>source height = $sourcefile_height
+<br>insert width = $insertfile_width
+<br>insert height = $insertfile_height
+
+<br>start point x = $dest_x
+<br>start point y = $dest_y";
+
     
-    imagedestroy($im);
-    imagedestroy($ims);
-    imagedestroy($im2);
+    imagedestroy($sourcefile_id);
+    imagedestroy($insertfile_id);
        
     die(0);
     ?>
