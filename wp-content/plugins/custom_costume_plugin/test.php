4c984eacba77a9cd91ce9f8eda69254ad884be65
STProgrammer <adam@adam-taylor.me.uk>
2014-05-02 07:12:32 +0100

    Why are my changes reverting when the laptop sleeps?

--- wp-content/plugins/custom_costume_plugin/test.php
+++ wp-content/plugins/custom_costume_plugin/test.php
@@ -1,53 +1,55 @@
 <?php
 
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
 error_reporting(-1);
 
-    echo('Starting script');
-    
-    //var_dump(gd_info());
-    phpinfo();
-    
-    echo getcwd() . "<br>";
-    
-//    $im = imagecreatefromjpeg(getcwd() . '/images/background.jpg');
-    $insertfile_id = imageCreateFromJPEG(getcwd() . '/images/head.jpg');
-    $sourcefile_id = imageCreateFromJPEG(getcwd() . '/images/background.jpg');
-    
-    $sourcefile_id2 = imagescale($sourcefile_id, 1500, 1190, IMG_BICUBIC_FIXED);
-    
-//    $im2 = imagecreatefromjpeg(getcwd() . '/images/head.jpg');
-    $sourcefile_width=imageSX($sourcefile_id2);
-    $sourcefile_height=imageSY($sourcefile_id2);
-    $insertfile_width=imageSX($insertfile_id);
-    $insertfile_height=imageSY($insertfile_id); 
-    
-    $max_height = 80;
-    $max_width = 80;
-    if($insertfile_width > $max_width || $insertfile_height > $max_width) {
-    $aspect = $insertfile_width / $insertfile_height;
-    if($insertfile_width > $max_width) {
-           $width = $max_width * $aspect;
-           $height = $max_height;
-    } else {
-        $width = $max_width;
-        $height = $max_height / $aspect;
-    }
-    
-  }
-    $insertfile_id2 = imagescale($insertfile_id, $width, $height, IMG_BICUBIC_FIXED);
-  
-//    imagecopymerge( $im , $im2 , 31 , 400 , 0 , 0 , imagesx($im2) , imagesy($im2) , 100 );
-    $dest_x = ( $sourcefile_width / 2 ) - ( $insertfile_width / 2 );
-    $dest_y = ( $sourcefile_height / 2 ) - ( $insertfile_height / 2 ); 
-    imageCopyMerge($sourcefile_id2, $insertfile_id2, 31, 400, 0, 0, $width, $height,100);
-    
-    echo('Image merged ');  
-    
-    $success = imagejpeg($sourcefile_id2, getcwd() . '/images/test.jpg', 75);
+echo('Starting script');
 
-echo('Image saved <br>');    
+//var_dump(gd_info());
+phpinfo();
+
+echo getcwd() . "<br>";
+
+//    $im = imagecreatefromjpeg(getcwd() . '/images/background.jpg');
+$insertfile_id = imageCreateFromJPEG(getcwd() . '/images/head.jpg');
+$sourcefile_id = imageCreateFromJPEG(getcwd() . '/images/background.jpg');
+
+$sourcefile_id2 = imagescale($sourcefile_id, 1500, 1190, IMG_BICUBIC_FIXED);
+
+//    $im2 = imagecreatefromjpeg(getcwd() . '/images/head.jpg');
+$sourcefile_width = imageSX($sourcefile_id2);
+$sourcefile_height = imageSY($sourcefile_id2);
+$insertfile_width = imageSX($insertfile_id);
+$insertfile_height = imageSY($insertfile_id);
+
+$max_size = 160;
+$aspect = $insertfile_width / $insertfile_height;
+if ($aspect > 1) {
+    $width = $max_size;
+    $height = $max_size / $aspect;
+} elseif ($aspect < 1) {
+    $width = $max_width * $aspect;
+    $height = $max_height;
+} else {
+    $width = $max_size;
+    $height = $max_size;
+}
+
+
+$insertfile_id2 = imagescale($insertfile_id, $width, $height, IMG_BICUBIC_FIXED);
+
+//    imagecopymerge( $im , $im2 , 31 , 400 , 0 , 0 , imagesx($im2) , imagesy($im2) , 100 );
+$dest_x = ( $sourcefile_width / 2 ) - ( $insertfile_width / 2 );
+$dest_y = ( $sourcefile_height / 4 ) - ( $insertfile_height / 2 );
+
+imageCopyMerge($sourcefile_id2, $insertfile_id2, $dest_x, $dest_y, 0, 0, $width, $height, 100);
+
+echo('Image merged ');
+
+$success = imagejpeg($sourcefile_id2, getcwd() . '/images/test.jpg', 75);
+
+echo('Image saved <br>');
 print "original:<hr><img src=\"$sourcefile_id\" width=\"300\"><br><br><br>new:<hr><img src=\"getcwd() . '/images/test.jpg'\">
 <br>source width = $sourcefile_width
 <br>source height = $sourcefile_height
@@ -57,10 +59,10 @@ print "original:<hr><img src=\"$sourcefile_id\" width=\"300\"><br><br><br>new:<h
 <br>start point x = $dest_x
 <br>start point y = $dest_y";
 
-    
-    imagedestroy($sourcefile_id);
-    imagedestroy($sourcefile_id2);
-    imagedestroy($insertfile_id);
-       
-    die(0);
-    ?>
+
+imagedestroy($sourcefile_id);
+imagedestroy($sourcefile_id2);
+imagedestroy($insertfile_id);
+
+die(0);
+?>
